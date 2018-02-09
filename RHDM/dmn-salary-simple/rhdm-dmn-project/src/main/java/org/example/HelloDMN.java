3c2f2e472aaf8b8f177587affc780f55b4e908a4
William Siqueira <william.fatecsjc@gmail.com>
2018-02-09 17:56:36 -0200

    Adding a DMN simple client

---
+++ RHDM/dmn-salary-simple/rhdm-dmn-project/src/main/java/org/example/HelloDMN.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.dmn.api.core.DMNContext;
+import org.kie.dmn.api.core.DMNDecisionResult;
+import org.kie.dmn.api.core.DMNModel;
+import org.kie.dmn.api.core.DMNResult;
+import org.kie.dmn.api.core.DMNRuntime;
+
+public class HelloDMN {
+
+	private static final String MODEL_ID = "Vacation Day Count - Solution 1";
+	private static final String NAMESPACE = "http://www.trisotech.com/definitions/_b2ef74f8-3453-44f7-887a-86ea1dd43d80";
+
+	public static void main(String[] args) {
+		// KIE API 
+		KieContainer kc = KieServices.Factory.get().getKieClasspathContainer();
+		KieBase kb = kc.getKieBase();
+		KieSession ks = kb.newKieSession();
+		
+		// Load the runtime, the model and create a new context
+		DMNRuntime dmnRuntime = ks.getKieRuntime(DMNRuntime.class);
+		DMNModel dmnModel = dmnRuntime.getModel(NAMESPACE, MODEL_ID);
+		DMNContext dmnContext = dmnRuntime.newContext();
+		
+		// Set input values on a context
+		dmnContext.set("Years of Service", 20);
+		dmnContext.set("Age", 32);
+		
+		// Evaluate the context on a model
+		DMNResult dmnResult = dmnRuntime.evaluateAll(dmnModel, dmnContext);
+		
+		// get a specific result and display it
+		DMNDecisionResult result = dmnResult.getDecisionResultByName("Number of vacation");
+		System.out.println("Number of vacation is: " + result.getResult());
+	}
+
+}
